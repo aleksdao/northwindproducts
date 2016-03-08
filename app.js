0bf25c3a733c0490e25b3424b6524d08b4dc86b3
Eric P. Katz <ericpkatz@gmail.com>
2016-03-07 20:54:22 -0500

    watch semi colons-- put next in correct spot-- use local bootstrap

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 var express = require("express");
-var app = express();
 var swig = require("swig");
 var path = require("path");
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json());//needed?
+
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "html");
 app.engine("html", swig.renderFile);
 
 app.use(require('method-override')('_method'));
 
-app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/products", require("./routes/products"));
 
 app.get("/", function(req, res, next) {
-  res.render("index")
+  res.render("index");
 });
 
-app.listen(3000);
+app.use(function(err, req, res, next){
+  console.log(err);
+  res.sendStatus(500);
+});
+
+app.listen(3000);//separate out server into another file.. server.js
--- routes/products.js
+++ routes/products.js
@@ -1,76 +1,57 @@
-var router = require("express").Router()
-var Product = require("../models/products")
+var router = require("express").Router();
+var Product = require("../models/products");
 router.use(require('method-override')('_method'));
 
 
 router.get("/", function(req, res, next) {
-  Product
-  .find({})
-  .exec().
-  then(function(products) {
-    console.log(products);
-    res.render("products", {products: products});
-  })
-  // res.render("products");
-})
-
-router.get("/active", function(req, res, next) {
-  var products;
-  Product
-  .find({active: true})
-  .exec()
-  .then(function(products) {
-    console.log(products)
-    res.render("products", {products: products})
-  })
-
+  Product.find({})
+    .then(function(products) {
+      console.log(products);
+      res.render("products", {products: products});
+    }, next);
 });
 
-router.put("/:product/quantity", function(req, res, next) {
-  Product
-  .findOne({ name: req.params.product})
-  .exec()
-  .then(function(product) {
-    console.log(product)
-    product.quantity = req.body.quantity
-    return product.save()
-  })
-  .then(function(product) {
-    res.redirect("/products");
-  })
-  .then(null, next);
-})
+router.get("/active", function(req, res, next) {
+  Product.find({active: true})
+    .then(function(products) {
+      res.render("products", {products: products});
+    }, next);
+});
 
-router.put("/:product/available", function(req, res, next) {
+router.put("/:name/quantity", function(req, res, next) {
+  Product.findOne({ name: req.params.name})
+    .then(function(product) {
+      product.quantity = req.body.quantity;
+      return product.save();
+    })
+    .then(function(product) {
+      res.redirect("/products");
+    }, next);
+});
+
+router.put("/:name/available", function(req, res, next) {
   Product
-  .findOne({ name: req.params.product })
-  .exec()
+  .findOne({ name: req.params.name })
   .then(function(product) {
-    if(product.active) product.active = false;
-    else product.active = true;
-    return product.save()
+    product.active = !product.active;
+    return product.save();
   })
   .then(function(product) {
     res.redirect("/products");
-  })
-  .then(null, next);
-})
+  }, next);
+});
 
 router.post("/", function(req, res, next) {
-  // res.redirect("../");
   var name = req.body.name;
   var description = req.body.description;
   var product = new Product({
     name: name,
     description: description
   });
-  console.log(product);
-  product
-  .save()
-  .then(function(savedProduct) {
-    res.redirect("/products")
-  })
-  .then(null, next);
-})
+  product.save()
+    .then(function(savedProduct) {
+      res.redirect("/products");
+    }, next);
+});
 
 module.exports = router;
